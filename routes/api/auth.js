f077e5044a86087e44706255002bc5e92902d187
Ashrith <ashrith19999@gmail.com>
2021-11-04 18:23:19 +0530

    Resolved vulnerabilities

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const config = require('config');
 const auth = require('../../middleware/auth');
 const User = require('../../models/User');
-const { check, validationResult } = require('express-validator/check');
+const { check, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
